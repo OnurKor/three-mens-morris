734c7e05ceb89e272449517d7f8191955c626d96
Onur <onurkordogan@gmail.com>
2022-12-17 23:34:45 +0300

    clean

--- src/App.js
+++ src/App.js
@@ -1,26 +1,246 @@
+import {useReducer } from "react";
+import GameBoard from "./GameBoard";
 
-import { useState } from 'react';
-import './App.css';
-import Finish from './components/Finish';
-import Main from './components/Main';
+const isPlacementComplete = (board) => {
+  let count = 0;
+  for (let row of board) {
+    for (let cell of row) {
+      if (cell !== null) {
+        count++;
+      }
+    }
+  }
+  return count === 6;
+};
+
+const isAdjacent = (row1, cell1, row2, cell2) => {
+  return (
+    (row1 === row2 && Math.abs(cell1 - cell2) === 1) ||
+    (cell1 === cell2 && Math.abs(row1 - row2) === 1)
+  );
+};
+
+const isCurrentPlayerPiece = (state, row, cell) => {
+  return state.board[row][cell] === state.current_player;
+};
+
+const checkForWinner = (state) => {
+  const { board, current_player } = state;
+  const winning_combinations = [
+    [
+      [0, 0],
+      [0, 1],
+      [0, 2],
+    ],
+    [
+      [1, 0],
+      [1, 1],
+      [1, 2],
+    ],
+    [
+      [2, 0],
+      [2, 1],
+      [2, 2],
+    ],
+    [
+      [0, 0],
+      [1, 0],
+      [2, 0],
+    ],
+    [
+      [0, 1],
+      [1, 1],
+      [2, 1],
+    ],
+    [
+      [0, 2],
+      [1, 2],
+      [2, 2],
+    ],
+    [
+      [0, 0],
+      [1, 1],
+      [2, 2],
+    ],
+    [
+      [0, 2],
+      [1, 1],
+      [2, 0],
+    ],
+  ];
+  for (let combination of winning_combinations) {
+    let count = 0;
+    for (let [row, cell] of combination) {
+      if (board[row][cell] === current_player) {
+        count++;
+      }
+    }
+    if (count === 3) {
+      return current_player;
+    }
+  }
+  return null;
+};
+
+const boardHasAtLeastPieces = (board, count) => {
+  let pieces = 0;
+  for (let row of board) {
+    for (let cell of row) {
+      if (cell !== null) {
+        pieces++;
+      }
+    }
+  }
+  return pieces >= count;
+};
+
+const placePiece = (state, { row_index, cell_index }) => {
+  const cell = state.board[row_index][cell_index];
+  if (cell === null) {
+    const new_board = [...state.board];
+    new_board[row_index][cell_index] = state.current_player;
+
+    
+    const new_is_placement_phase = isPlacementComplete(new_board)
+      ? false
+      : true;
+    const new_state = {
+      ...state,
+      board: new_board,
+      current_player: state.current_player === "a" ? "b" : "a",
+      is_placement_phase: new_is_placement_phase,
+    };
+    
+    if (boardHasAtLeastPieces(new_board, 5)) {
+      console.log("checking for winner");
+     
+      const winner = checkForWinner({
+        ...new_state,
+        current_player: state.current_player,
+      });
+      if (winner) {
+        return {
+          ...new_state,
+          winner,
+        };
+      }
+    }
+
+    return new_state;
+  }
+  return state;
+};
+
+const selectPiece = (state, { row_index, cell_index }) => {
+  const cell = state.board[row_index][cell_index];
+  if (cell === state.current_player) {
+    console.log("selecting piece");
+    return {
+      ...state,
+      selected_piece: [row_index, cell_index],
+    };
+  }
+  return state;
+};
+
+const moveSelectedPiece = (state, { row_index, cell_index }) => {
+  console.log("moving piece");
+  const [selected_row, selected_cell] = state.selected_piece;
+  const cell = state.board[row_index][cell_index]; 
+  if (
+    cell === null &&
+    isAdjacent(selected_row, selected_cell, row_index, cell_index) &&
+    isCurrentPlayerPiece(state, selected_row, selected_cell)
+  ) {
+    
+    const new_board = [...state.board];
+    new_board[selected_row][selected_cell] = null;
+    new_board[row_index][cell_index] = state.current_player;
+    const new_state = {
+      ...state,
+      board: new_board,
+      selected_piece: null,
+      current_player: state.current_player === "a" ? "b" : "a",
+    };
+
+    // check for winner
+    const winner = checkForWinner({
+      ...new_state,
+      current_player: state.current_player,
+    });
+    if (winner) {
+      return {
+        ...new_state,
+        winner,
+      };
+    }
+
+    return new_state;
+  }
+  return state;
+};
 
 
-function App() {
+const reducer = (state, { type, payload }) => {
+  if (state.is_placement_phase) {
+    
+    return placePiece(state, payload); 
+  }
+  
+  if (state.selected_piece !== null) {
+    
+    if (
+      state.selected_piece[0] === payload.row_index &&
+      state.selected_piece[1] === payload.cell_index
+    ) {
+      return {
+        ...state,
+        selected_piece: null,
+      };
+    }
 
-  const [gameScreen, setGameScreen] = useState("start");
+    
+    if (isCurrentPlayerPiece(state, payload.row_index, payload.cell_index)) {
+      return selectPiece(state, payload);
+    }
 
-  const restartGame = () => {
-    setGameScreen("start");
-  };
+    
+    return moveSelectedPiece(state, payload);
+  } else {
+    return selectPiece(state, payload);
+  }
+};
+
+const App = () => {
+ 
+
+  const [state, dispatch] = useReducer(reducer, {
+    board: [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ],
+    current_player: "a",
+    is_placement_phase: true,
+    selected_piece: null, 
+    instruction_text: "Player 1, Place your stones",
+    winner: null,
+  });
 
   return (
-     <div>
-      <div className="App">  
-        <Main onRestart={restartGame} />
-        {gameScreen === "finish" && <Finish onRestart={restartGame} />}
+    <div className="App">
+      <div className="game-button">
+        <button onClick={() => window.location.reload()}>Restart</button>
       </div>
+
+      <GameBoard
+        board={state.board}
+        selected_cell={state.selected_piece}
+        dispatch={dispatch}
+        winner={state.winner}
+      />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/GameBoard.js
@@ -0,0 +1,70 @@
+import player1image from "./assets/white.png";
+import player2image from "./assets/black.webp";
+
+const player1 = (
+ <img style={{width:"50px", height:"50px"}} src={player1image} alt="" />
+);
+const player2 = (
+ <img style={{width:"50px", height:"50px"}} src={player2image} alt="" />
+);
+
+const GameBoard = ({ board, winner, selected_cell, dispatch }) => {
+  const displayPlayer = (player) => {
+    if (player === "a") {
+      return "Player A";
+    } else {
+      return "Player B";
+    }
+  };
+
+  return (
+    <div>
+      <h1 style={{ color: "white" }}>WELCOME!</h1>
+      <h1 style={{ color: "white" }}>Three Men's Morris</h1>
+      {winner && (
+        <h1 style={{ color: "green" }}>Winner: {displayPlayer(winner)}</h1>
+      )}
+
+      <div className={`board ${winner ? "disabled" : ""}`}>
+        {board.map((row, row_index) => {
+          return (
+            <div className="row" key={row_index}>
+              {row.map((cell, cell_index) => {
+                return (
+                  <div
+                    className={`cell ${
+                      selected_cell &&
+                      selected_cell[0] === row_index &&
+                      selected_cell[1] === cell_index
+                        ? "selected"
+                        : ""
+                    }`}
+                    key={cell_index}
+                    onClick={() => {
+                      if (winner) {
+                        return;
+                      } else {
+                        dispatch({
+                          type: "CELL_CLICKED",
+                          payload: {
+                            row_index,
+                            cell_index,
+                            difficulty: "normal",
+                          },
+                        });
+                      }
+                    }}
+                  >
+                    {cell === "a" ? player2 : cell === "b" ? player1 : ""}
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default GameBoard;
